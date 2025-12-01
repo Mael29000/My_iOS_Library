4157f05fc3043435c0da1aab532b5634887ad885
Mael29000 <maelsuard@orange.fr>
2025-12-01 21:45:50 +0100

    Initial Commit

---
+++ My_iOS_Library/My_iOS_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_iOS_LibraryApp.swift
+//  My_iOS_Library
+//
+//  Created by Maël Suard on 01/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct My_iOS_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
